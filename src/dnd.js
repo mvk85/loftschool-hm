be6e5896ed8710906952493b691c65b0cd281cbe
max <messages.mvk@gmail.com>
2017-02-17 17:43:35 +0300

    done hm5 (5.1 and 5.2)

--- src/dnd.js
+++ src/dnd.js
@@ -15,7 +15,19 @@
  * @example
  * homeworkContainer.appendChild(...);
  */
+
+const CONTAIN_WIDTH = 800;
+const CONTAIN_HEIGHT = 500;
+
 let homeworkContainer = document.querySelector('#homework-container');
+let container = document.getElementById('homework-container');
+
+container.style.height = CONTAIN_HEIGHT + 'px';
+container.style.width = CONTAIN_WIDTH + 'px';
+container.style.position = 'relative';
+container.style.borderWidth = '1px';
+container.style.borderColor = '#000';
+container.style.borderStyle = 'solid';
 
 /**
  * Функция должна создавать и возвращать новый div с классом draggable-div и случайными размерами/цветом/позицией
@@ -25,6 +37,35 @@ let homeworkContainer = document.querySelector('#homework-container');
  * @return {Element}
  */
 function createDiv() {
+    let div = document.createElement('div');
+    let params = {
+        size: { width: randomNum(200), height: randomNum(100) },
+        backgroundColor: `#${randomNum(1000000)}`,
+        position: { left: randomNum(CONTAIN_WIDTH), top: randomNum(CONTAIN_HEIGHT) }
+    };
+
+    if (params.size.width + params.position.left > CONTAIN_WIDTH) {
+        params.position.left = CONTAIN_WIDTH - params.size.width;
+    }
+
+    if (params.size.height + params.position.top > CONTAIN_HEIGHT) {
+        params.position.top = CONTAIN_HEIGHT - params.size.height;
+    }
+
+    div.classList.add('draggable-div');
+    div.style.position = 'absolute';
+    div.style.width = params.size.width + 'px';
+    div.style.height = params.size.height + 'px';
+    div.style.left = params.position.left + 'px';
+    div.style.top = params.position.top + 'px';
+    div.style.backgroundColor = params.backgroundColor;
+    div.style.cursor = 'pointer';
+    
+    function randomNum(size) {
+        return Math.floor(Math.random()*size);
+    }
+
+    return div;
 }
 
 /**
@@ -33,6 +74,48 @@ function createDiv() {
  * @param {Element} target
  */
 function addListeners(target) {
+    homeworkContainer.addEventListener('mousedown', (event) => {
+        if (event.target != target) {
+            return;
+        }
+        
+        let box = target.getBoundingClientRect();
+        let shift = {
+            X: event.pageX - box.left,
+            Y: event.pageY - box.top
+        }
+
+        let handlerMove = (event) => {
+            let top = event.pageY - shift.Y;
+            let left = event.pageX - shift.X;
+
+            if ((top + box.height) > CONTAIN_HEIGHT) {
+                top = CONTAIN_HEIGHT - box.height;
+            } else if (top < 0) {
+                top = 0;
+            }
+
+            if ((left + box.width) > CONTAIN_WIDTH) {
+                left = CONTAIN_WIDTH - box.width;
+            } else if (left < 0) {
+                left = 0;
+            }
+
+            target.style.top = top + 'px';
+            target.style.left = left + 'px';
+        };
+        let handlerUp = () => {
+            homeworkContainer.removeEventListener('mousemove', handlerMove);
+            homeworkContainer.removeEventListener('mouseup', handlerUp);
+        }
+
+        homeworkContainer.addEventListener('mousemove', handlerMove);
+        homeworkContainer.addEventListener('mouseup', handlerUp)
+
+    })
+
+    homeworkContainer.addEventListener('dragstart', (event) => event.preventDefault());
+
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
